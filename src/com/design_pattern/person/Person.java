6c223fba4d2e8b46926f3f611c1cf683e7f20e11
kaushik-gopu1998 <kaushikgopu1998@gmail.com>
2023-06-27 22:57:03 -0600

    proxy

---
+++ src/com/design_pattern/person/Person.java
@@ -0,0 +1,12 @@
+package com.design_pattern.person;
+
+public interface Person {
+	String getName();
+	String getGender();
+	String getInterests();
+	int getGeekRating();
+	void setName(String name);
+	void setGender(String gender);
+	void setInterests(String interests);
+	void setGeekRating(int rating);
+}
